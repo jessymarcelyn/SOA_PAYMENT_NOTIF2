39bb4163564b1d36f6e03bca43fd6bf79e28f985
c14210065 <c14210065@john.petra.ac.id>
2024-06-26 21:05:05 +0700

    Update init-db.sql

--- testing_api/payment_notif/mysql-init/init-db.sql
+++ testing_api/payment_notif/mysql-init/init-db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2024 at 12:44 PM
+-- Generation Time: Jun 26, 2024 at 04:02 PM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
